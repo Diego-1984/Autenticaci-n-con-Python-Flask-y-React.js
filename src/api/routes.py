25625655ea9a8fe0de069f0e6eaace567e1bfb07
Diego-1984 <diegomenoruceta@gmail.com>
2023-04-17 11:44:11 +0000

    Iniciado private js

--- src/api/routes.py
+++ src/api/routes.py
@@ -15,27 +15,23 @@ def handle_hello():
         "message": "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
     }
 
-    @api.route('/signup', methods=['POST'])
-    def create_new_user():
-        email = request.json.get('email', None)
-        password = request.json.get('password', None)
-        is_active = request.json.get('is_active', True)
-        
-        user=User(email=email, password=password, is_active=is_active)
-        db.session.add(user)
-        db.session.commit()
-        response_body={
-            "message": "all ok"
+@api.route('/signup', methods=['POST'])
+def create_new_user():
+    email = request.json.get('email', None)
+    password = request.json.get('password', None)
+    is_active = request.json.get('is_active', True)
     
-        }
+    user=User(email=email, password=password, is_active=is_active)
+    db.session.add(user)
+    db.session.commit()
+    response_body={
+        "message": "all ok"
 
-        return jsonify(response_body), 200
+    }
 
-    @api.route('/login', methods=['POST'])
-    def login_user():
-        email = request.json.get('email', None)
-        password = request.json.get('password', None)
+    return jsonify(response_body), 200
 
-    @api.route('/private', methods=['GET'])
-    def private_route():
-       
+@api.route('/login', methods=['POST'])
+def login_user():
+    email = request.json.get('email', None)
+    password = request.json.get('password', None)
--- src/front/js/pages/private.js
+++ src/front/js/pages/private.js
@@ -1,15 +1,15 @@
-import React, { useContext } from "react";
-import { Context } from "../store/appContext";
-import rigoImageUrl from "../../img/rigo-baby.jpg";
-import "../../styles/home.css";
+import React, { useEffect } from "react";
 
-export const Home = () => {
-	const { store, actions } = useContext(Context);
+const Private =()=>{
+	useEffect(()=>{
+		
+	}, []);
 
-	return (
-		<div className="text-center mt-5">
-			<h1>PRIVATE</h1>
-			
-		</div>
-	);
-};
+	return(
+		<>
+
+
+		</>
+	)
+}
+export default Private
